d4bcc6c6c2120386f325c707c5961ca3c6d22a99
Giorgos Iliopoulos <gio.giorgaras@gmail.com>
2022-05-22 18:38:49 +0300

    Innit of project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import donwloadIcon from "./assets/dl.png";
+import diavgeiaLogo from "./assets/diavgeia.png"
 
-function App() {
+export default function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const afm = process.env.REACT_APP_AFM;
+    fetch(
+      `https://diavgeia.gov.gr/opendata/search/advanced.json?q=receiverAFM:"${afm}"`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        let decisions = actualData.decisions;
+        console.log(decisions);
+        setData(decisions);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData(null);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <a href={"https://diavgeia.gov.gr"} target="_blank" className="d-flex justify-content-center" rel="noreferrer"><img src={diavgeiaLogo} alt='diavgeia'/></a>
+        {error && (
+          <div>{`There is a problem fetching the post data - ${error}`}</div>
+        )}
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Τίτλος</th>
+              <th scope="col">Αριθμός πρωτοκόλλου</th>
+              <th scope="col">ΑΔΑ</th>
+              <th scope="col">Υπογραφή με</th>
+              <th scope="col">Αρχείο</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data.map(
+                ({
+                  ada,
+                  subject,
+                  protocolNumber,
+                  extraFieldValues,
+                  documentUrl,
+                }) => (
+                  <tr>
+                    <td>{subject}</td>
+                    <td>{protocolNumber}</td>
+                    <td>{ada}</td>
+                    <td>{extraFieldValues.org.name}</td>
+                    <td className="filetd">
+                      <a href={documentUrl} rel="noreferrer" target={"_blank"}>
+                        <img className="dlicon" alt="dl" src={donwloadIcon} />
+                      </a>
+                    </td>
+                  </tr>
+                )
+              )}
+          </tbody>
+        </table>
+      </Container>
     </div>
   );
 }
-
-export default App;
